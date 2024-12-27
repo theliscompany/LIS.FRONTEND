650f39b759b25496e6740695e6cda2f820d56221
Aloys Kemche <aloysterano@live.com>
2024-12-27 10:14:58 -0500

    Implementation get shipments

--- QUOTE.UI_VITE/LIS.QUOTES.UI/src/App.tsx
+++ QUOTE.UI_VITE/LIS.QUOTES.UI/src/App.tsx
@@ -6,19 +6,27 @@ import './locales/i18n';
 import Shipments from './pages/Shipments';
 import Layout from './layout/Layout';
 import BackendServiceProvider from './api/api';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
 
   return (
     <BrowserRouter>
         <AuthenticatedTemplate>
-          <BackendServiceProvider>
-            <Routes>
-              <Route path='/*' element={<Layout />}>
-                <Route path='' element={<Shipments />} />
-              </Route>
-            </Routes>
-          </BackendServiceProvider>
+          <QueryClientProvider client={queryClient}>
+            <BackendServiceProvider>
+              <Routes>
+                <Route path='/*' element={<Layout />}>
+                  <Route path='' element={<Shipments />} />
+                </Route>
+              </Routes>
+            </BackendServiceProvider>
+            <ReactQueryDevtools />
+          </QueryClientProvider>
+          
           
         </AuthenticatedTemplate>
         <UnauthenticatedTemplate>
