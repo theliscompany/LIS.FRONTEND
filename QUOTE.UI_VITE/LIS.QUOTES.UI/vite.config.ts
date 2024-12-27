5adc9df60cbb2add23c8df7296749c290bddd37b
Aloys Kemche <aloysterano@live.com>
2024-12-27 17:36:53 -0500

    Resolve bug

--- QUOTE.UI_VITE/LIS.QUOTES.UI/vite.config.ts
+++ QUOTE.UI_VITE/LIS.QUOTES.UI/vite.config.ts
@@ -4,21 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'vendor-react'; // Séparer le code lié à React
-            }
-            if (id.includes('lodash')) {
-              return 'vendor-lodash'; // Séparer Lodash
-            }
-            return 'vendor'; // Autres bibliothèques
-          }
-        },
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules')) {
+  //           if (id.includes('react') || id.includes('react-dom')) {
+  //             return 'vendor-react'; // Séparer le code lié à React
+  //           }
+  //           if (id.includes('lodash')) {
+  //             return 'vendor-lodash'; // Séparer Lodash
+  //           }
+  //           return 'vendor'; // Autres bibliothèques
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
 })
